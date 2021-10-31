51786e0920c7fba94e8bf966a0950f058767ccab
Reham Albakouni <albakoum@ualberta.ca>
2021-10-31 13:53:09 -0600

    add addCity test (should fail)

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -47,4 +47,21 @@ public class CustomList extends ArrayAdapter<City> {
         return view;
 
     }
+
+    /**
+     * this function will get the size of list
+     * @return
+     */
+    public int getCount(){
+        return cities.size();
+    }
+
+    /**
+     * this function will add city object into the list
+     * @return
+     */
+    public void addCity(City city){
+        cities.add(city);
+        return;
+    }
 }
